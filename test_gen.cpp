791c128a298a7199adabaca748891698dd624192
Selina Liu <liu15@seas.upenn.edu>
2015-11-30 01:37:02 -0500

    Added testing for grids of all difficulty levels for completeness

--- test_gen.cpp
+++ test_gen.cpp
@@ -4,7 +4,12 @@
 #include <iostream>
 
 int main(){
+    // Generates a grid for each difficulty level
 	Grid sample = Generator::getGrid(1);
+	sample.print();
+	sample = Generator::getGrid(2);
+	sample.print();
+	sample = Generator::getGrid(3);
 	sample.print();
 	std::cout << "All Tests Pass!" << std::endl;
 	return 0;
